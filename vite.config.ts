2e1175cf7e0c065a1b27447b6d00bcf1f23d2d79
thomas-x-69 <thomas.ashraf975@gmail.com>
2025-01-14 17:14:48 +0200

    Initial setup: Manifest V3

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,24 @@
 import { defineConfig } from "vite";
+import { crx } from "@crxjs/vite-plugin";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import manifest from "./manifest.json";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(), // Handles React-specific features like JSX transformation
+    crx({ manifest }), // Handles Chrome extension specific builds
+  ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "./src"), // Enables clean imports
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        popup: "index.html",
+      },
     },
   },
 });
